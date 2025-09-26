d2e8ae4d4cbcb7e2237da4310065d510911cc219
surajgojaur <surajgojanur@gmail.com>
2025-09-26 20:53:41 +0530

    worked on mutable string

---
+++ mutableString.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    char *st="Hello world";
+    printf("%s",st);
+    
+    st="Suraj";
+    printf("%s",st);
+
+}
+
+// #include<stdio.h>
+// int main(){
+//     char *st="Hello world";
+//     puts(st);
+//     st="Suraj";
+//     puts(st);
+
+// }
